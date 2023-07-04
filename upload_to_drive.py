0f50d5168fab0ba756b1964280e882730ea1673b
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-04 19:39:25 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -8,6 +8,7 @@ from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from download import download_zoom_recordings
+import threading
 
 # Set up Flask app
 upload_blueprint = Blueprint('upload', __name__)
@@ -31,38 +32,29 @@ flow = Flow.from_client_secrets_file(
     redirect_uri='https://flask-production-d5a3.up.railway.app/upload_callback'  # Replace with your domain
 )
 
-# Redirect user to Google for authentication
-@upload_blueprint.route('/')
-def index():
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        include_granted_scopes='true'
-    )
-    return redirect(authorization_url)
-
-def uploadVideos(drive_service):
+# Background worker for uploading videos
+def background_upload_videos(drive_service):
     access_token = session.get('zoom_access_token')
     recordings = download_zoom_recordings(access_token)
-
-    # Iterate over the recordings
-    for recording in recordings:
-        for files in recording['recording_files']:
-            # Check if the status is "completed" and the file extension is "mp4"
-            if files['status'] == 'completed' and files['file_extension'] == 'MP4':
-                # Fetch the video file from the download URL
-                download_url = files['download_url']
-                
-                response = requests.get(download_url)
-                video_content = response.content
-                # Upload the video to Google Drive
-                file_name = recording.get('topic') + '.mp4'
-                file_metadata = {'name': file_name}
-                media = MediaIoBaseUpload(io.BytesIO(video_content), mimetype='video/mp4')
-                drive_service.files().create(
-                    body=file_metadata,
-                    media_body=media,
-                    fields='id'
-                ).execute()
+        # Iterate over the recordings
+        for recording in recordings:
+            for files in recording['recording_files']:
+                # Check if the status is "completed" and the file extension is "mp4"
+                if files['status'] == 'completed' and files['file_extension'] == 'MP4':
+                    # Fetch the video file from the download URL
+                    download_url = files['download_url']
+                    
+                    response = requests.get(download_url)
+                    video_content = response.content
+                    # Upload the video to Google Drive
+                    file_name = recording.get('topic') + '.mp4'
+                    file_metadata = {'name': file_name}
+                    media = MediaIoBaseUpload(io.BytesIO(video_content), mimetype='video/mp4')
+                    drive_service.files().create(
+                        body=file_metadata,
+                        media_body=media,
+                        fields='id'
+                    ).execute()
 
 # Callback route after authentication
 @upload_blueprint.route('/upload_callback')
@@ -76,6 +68,10 @@ def upload_callback():
     # Create a Google Drive service instance using the credentials
     credentials = flow.credentials
     drive_service = build('drive', API_VERSION, credentials=credentials)
-    uploadVideos(drive_service)
-   
-    return 'Video uploaded successfully!'
+
+    # Start the background worker to upload videos
+    worker_thread = threading.Thread(target=background_upload_videos, args=(drive_service,))
+    worker_thread.start()
+
+    return 'Video upload started in the background!'
+
