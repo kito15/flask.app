293b0644c2479cd99a158661025d431d9c31ce25
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-03 22:55:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, session, request, jsonify
 from zoom_authorize import zoom_blueprint
+from upload_to_drive import upload_blueprint
 
 # Create Flask app
 app = Flask(__name__)
@@ -7,3 +8,4 @@ app.secret_key = '@unblinded2018'
 
 # Register blueprints
 app.register_blueprint(zoom_blueprint)
+app.register_blueprint(upload_blueprint)
