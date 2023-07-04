eb430775495b36e10df63e21f47465545107efe4
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-04 19:00:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import rq
+from rq import Queue
+from rq_scheduler import Scheduler
+from datetime import datetime, timedelta
 from flask import Flask, Blueprint, session, request, jsonify
 from upload_to_drive import upload_blueprint
 from zoom_authorize import zoom_blueprint
@@ -5,6 +9,8 @@ from zoom_authorize import zoom_blueprint
 # Create Flask app
 app = Flask(__name__)
 app.secret_key = '@unblinded2018'
+q = Queue(connection=rq.Connection())
+scheduler = Scheduler(queue=q)
 
 # Register blueprints
 app.register_blueprint(zoom_blueprint)
