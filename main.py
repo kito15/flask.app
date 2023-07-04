4b2bfab22045cc40e413941bd5c7a9fcc194a432
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-03 22:34:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,3 @@ app.secret_key = '@unblinded2018'
 
 # Register blueprints
 app.register_blueprint(zoom_blueprint)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
