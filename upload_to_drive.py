4ea88b7c703fdcc007ff0f17b6bbb1ea195123c4
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-04 18:52:39 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -8,7 +8,6 @@ from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from download import download_zoom_recordings
-from celery import Celery
 
 # Set up Flask app
 upload_blueprint = Blueprint('upload', __name__)
@@ -25,20 +24,12 @@ SCOPES = [
     'https://www.googleapis.com/auth/userinfo.profile'
 ]
 API_VERSION = 'v3'
-
 # Create the Flow instance
 flow = Flow.from_client_secrets_file(
     CLIENT_SECRETS_FILE,
     scopes=SCOPES,
     redirect_uri='https://flask-production-d5a3.up.railway.app/upload_callback'  # Replace with your domain
 )
-
-# Create a Celery app
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = os.environ.get('REDIS_URL')
-app.config['CELERY_RESULT_BACKEND'] = os.environ.get('REDIS_URL')
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
 
 # Redirect user to Google for authentication
 @upload_blueprint.route('/')
@@ -49,38 +40,21 @@ def index():
     )
     return redirect(authorization_url)
 
-
-# Callback route after authentication
-@upload_blueprint.route('/upload_callback')
-def upload_callback():
-    # Fetch the authorization code from the callback request
+def uploadVideos(drive_service):
     access_token = session.get('zoom_access_token')
     recordings = download_zoom_recordings(access_token)
-    authorization_code = request.args.get('code')
 
-    # Exchange the authorization code for a token
-    flow.fetch_token(authorization_response=request.url)
-
-    # Create a Google Drive service instance using the credentials
-    credentials = flow.credentials
-    drive_service = build('drive', API_VERSION, credentials=credentials)
-
-    # Process recordings in the background using Celery
-    process_recordings.delay(recordings, drive_service)
-
-    return 'Video upload task has started!'
-
-@celery.task
-def process_recordings(recordings, drive_service):
+    # Iterate over the recordings
     for recording in recordings:
         for files in recording['recording_files']:
             # Check if the status is "completed" and the file extension is "mp4"
             if files['status'] == 'completed' and files['file_extension'] == 'MP4':
                 # Fetch the video file from the download URL
                 download_url = files['download_url']
+                print(download_url)
+               
                 response = requests.get(download_url)
                 video_content = response.content
-
                 # Upload the video to Google Drive
                 file_name = recording.get('topic') + '.mp4'
                 file_metadata = {'name': file_name}
@@ -91,5 +65,19 @@ def process_recordings(recordings, drive_service):
                     fields='id'
                 ).execute()
 
-    return 'Video upload completed!'
+# Callback route after authentication
+@upload_blueprint.route('/upload_callback')
+def upload_callback():
+    # Fetch the authorization code from the callback request
+    authorization_code = request.args.get('code')
+    
+    # Exchange the authorization code for a token
+    flow.fetch_token(authorization_response=request.url)
 
+    # Create a Google Drive service instance using the credentials
+    credentials = flow.credentials
+    drive_service = build('drive', API_VERSION, credentials=credentials)
+
+    uploadVideos(drive_service)
+    
+    return 'Video uploaded successfully!'
