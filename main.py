b64247877e8cdeeb3f95053f0472402e38184713
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-03 22:25:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, Blueprint, session, request, jsonify
+from upload_to_drive import upload_blueprint
+from zoom_authorize import zoom_blueprint
 
+
+# Create Flask app
 app = Flask(__name__)
+app.secret_key = '@unblinded2018'
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+# Register blueprints
+app.register_blueprint(zoom_blueprint)
+app.register_blueprint(upload_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
