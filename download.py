e1d4eb714036eb23b74f3bbf340ae4c6e94a3ce1
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-03 22:55:05 -0400

    Create download.py

---
+++ download.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime, timedelta
+import json
+from flask import session
+
+def download_zoom_recordings(access_token):
+    
+    if not access_token:
+        print("Access token not found in session. Please authenticate with Zoom.")
+        return
+
+    headers = {"Authorization": "Bearer " + access_token}
+
+    end_date = datetime.now()
+    start_date = datetime(2023, 7, 3)
+
+    all_recordings = []
+    current_date = end_date
+
+    while current_date >= start_date:
+        prev_date = current_date - timedelta(days=30)  # Retrieve recordings in 30-day chunks
+        if prev_date < start_date:
+            prev_date = start_date
+
+        params = {
+            "from": prev_date.isoformat() + "Z",
+            "to": current_date.isoformat() + "Z",
+            "page_size": 300,  # Increase the page size to retrieve more recordings per page
+            "page_number": 1
+        }
+
+        while True:
+            response = requests.get(
+                "https://api.zoom.us/v2/accounts/me/recordings",
+                headers=headers,
+                params=params
+            )
+            recordings_json = response.json()
+            all_recordings.extend(recordings_json["meetings"])
+            total_records = recordings_json["total_records"]
+            records_per_page = recordings_json["page_size"]
+            total_pages = total_records // records_per_page
+
+            if total_records % records_per_page != 0:
+                total_pages += 1
+
+            if params["page_number"] >= total_pages:
+                break
+
+            params["page_number"] += 1
+
+        current_date = prev_date - timedelta(days=1)  # Move to the previous date range
+
+    return all_recordings
