4cc31cdb96dd6a070dda6dc032c71c84f4dbc09f
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-04 20:18:13 -0400

    Update celeryconfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,7 +1,7 @@
 # celeryconfig.py
 
-broker_url = 'redis://redis:6379/0'
-result_backend = 'redis://redis:6379/0'
+broker_url = 'redis://default:2QDSdWu0b2py954gMfcJ@containers-us-west-82.railway.app:6096'
+result_backend = 'redis://default:2QDSdWu0b2py954gMfcJ@containers-us-west-82.railway.app:6096'
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
