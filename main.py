e4e7f5fede1526323b8dc72c3ff296c6d5842dd3
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-05 15:54:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,3 @@ app.secret_key = '@unblinded2018'
 app.register_blueprint(zoom_blueprint)
 app.register_blueprint(upload_blueprint)
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    if request.method == "GET":
-        return jsonify({"response": "GET"})
-    elif request.method == "POST":
-        try:
-            data = request.get_json(force=True)
-            accountName= data.get('accountName')
-            email=data.get('email')
-            
-            # Store email and topic in session
-            session['email'] = email
-            session['accountName'] = accountName
-
-            print(email)
-            print(accountName)
-            
-            return jsonify(data)
-        except Exception as e:
-            return jsonify({"error": str(e)})
-
