2712e6b3cac487696b5ae59be684efdb00a5b9ab
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-05 12:44:20 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -143,7 +143,7 @@ def uploadFiles(drive_service):
                 
 # Callback route after authentication
 @upload_blueprint.route('/upload_callback')
-def upload_callback(data):
+def upload_callback():
     authorization_code = request.args.get('code')
 
     # Exchange the authorization code for a token
@@ -152,10 +152,7 @@ def upload_callback(data):
     # Create a Google Drive service instance using the credentials
     credentials = flow.credentials
     drive_service = build('drive', API_VERSION, credentials=credentials)
-
-    topic = data.get('topic')
-    email=data.get('email')
     
     uploadFiles(drive_service)
 
-    return data
+    return "Recordings are being uploaded"
