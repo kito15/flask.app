903032ff99aa3001ca11803ba0ce906900dd791d
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-04 20:22:27 -0400

    Delete celeryconfig.py

--- celeryconfig.py
+++
@@ -1,8 +0,0 @@
-# celeryconfig.py
-
-broker_url = 'redis://default:2QDSdWu0b2py954gMfcJ@containers-us-west-82.railway.app:6096'
-result_backend = 'redis://default:2QDSdWu0b2py954gMfcJ@containers-us-west-82.railway.app:6096'
-task_serializer = 'json'
-result_serializer = 'json'
-accept_content = ['json']
-timezone = 'UTC'
