fe4cf67dd25e109fbfb718f526f588033c04b1db
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 13:37:40 -0400

    Create celery.py

---
+++ celery.py
@@ -0,0 +1,13 @@
+from celery import Celery
+import os
+
+app = Celery('your_app_name', broker=os.getenv('REDIS_URL'), backend=os.getenv('REDIS_URL'))
+app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
+    task_routes={
+        'your_app_name.tasks.upload_task': {'queue': 'video_queue'}
+    },
+    worker_prefetch_multiplier=1
+)
