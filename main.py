4d37ed8722bcfcc1c496a0f68870d03c7d0facd6
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 09:54:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint, session, request, jsonify
-from upload_to_drive import upload_blueprint, store_parameters, retrieve_parameters
+from upload_to_drive import upload_blueprint, store_parameters, retrieve_parameters, retrieve_share_link
 from zoom_authorize import zoom_blueprint
 
 # Create Flask app
@@ -22,6 +22,7 @@ def test():
 
             results=store_parameters(accountName,email)
             print(retrieve_parameters())
+            print(retrieve_share_link())
             
             return jsonify(data)
         except Exception as e:
