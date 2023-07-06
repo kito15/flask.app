07579a54ec71a991311f54486183e580cc0af009
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 15:06:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, session, request, jsonify
 from upload_to_drive import upload_blueprint, store_parameters, retrieve_parameters
 from zoom_authorize import zoom_blueprint
+from celery import app
 from your_app_name import test_task
 
 # Create Flask app
