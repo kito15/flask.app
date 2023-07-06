0e7fe7a072fbad2eecf171bb36a3fee46cd381e6
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 10:59:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def test():
 
             results=store_parameters(accountName,email)
             print(retrieve_parameters())
-            print(call_retrieve_share_link())
+            print(upload_blueprint.after_request(call_retrieve_share_link))
             
             return jsonify(data)
         except Exception as e:
