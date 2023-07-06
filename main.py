95505afd8cea7afb698955ffc33418104b6002ff
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 15:46:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, Blueprint, session, request, jsonify
 from upload_to_drive import upload_blueprint, store_parameters, retrieve_parameters
 from zoom_authorize import zoom_blueprint
-from task import celery, test_task
 
 # Create Flask app
 app = Flask(__name__)
@@ -10,9 +9,6 @@ app.secret_key = '@unblinded2018'
 # Register blueprints
 app.register_blueprint(upload_blueprint)
 app.register_blueprint(zoom_blueprint)
-
-result = test_task.delay()
-print(result.get())
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
