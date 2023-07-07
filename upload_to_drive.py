9ba7f328501700545de4650c042cac2bb9025d65
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-07 10:22:06 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -64,17 +64,6 @@ def retrieve_parameters():
     global stored_params
     return stored_params
     
-def drive_service_serializer(obj):
-    from googleapiclient.discovery import Resource
-    if isinstance(obj, Resource):
-        # Serialize the drive_service object to JSON
-        serialized = obj._serialize()
-        return {
-            'serialized': serialized,
-            'class': obj.__class__.__name__,
-        }
-    return obj
-
 # Callback route after authentication
 @upload_blueprint.route('/upload_callback')
 def upload_callback():
@@ -93,10 +82,7 @@ def upload_callback():
     params=retrieve_parameters()
     accountName=params[0]
     email=params[1]
-
-    # Serialize the drive_service object to JSON
-    drive_service_json = json.dumps(drive_service, default=drive_service_serializer)
     
-    uploadFiles.delay(drive_service_json,recordings,accountName,email)
+    uploadFiles.delay(credentials,recordings,accountName,email)
     
     return "Recording are being uploaded"
