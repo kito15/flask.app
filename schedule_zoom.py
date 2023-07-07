61052c750aae17c0ffdbe282b7bddd867a7b50fa
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-07 16:12:04 -0400

    Update schedule_zoom.py

--- schedule_zoom.py
+++ schedule_zoom.py
@@ -1,14 +1,17 @@
 import requests
 import schedule
 import time
+import logging
+
+logging.basicConfig(filename='schedule_zoom.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def run_job():
     url = "https://flask-production-d5a3.up.railway.app/authorize"
     response = requests.get(url)
     if response.status_code == 200:
-        print("Job executed successfully")
+        logging.info("Job executed successfully")
     else:
-        print(f"Job failed with status code: {response.status_code}")
+        logging.error(f"Job failed with status code: {response.status_code}")
 
 # Schedule the job to run every hour
 schedule.every().hour.do(run_job)
@@ -17,4 +20,3 @@ schedule.every().hour.do(run_job)
 while True:
     schedule.run_pending()
     time.sleep(1)
-run_job()
