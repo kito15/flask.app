4bbf447bdd7b8edc6a17146508e0ea9156e75cd1
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-07 16:04:16 -0400

    Create schedule_zoom.py

---
+++ schedule_zoom.py
@@ -0,0 +1,19 @@
+import requests
+import schedule
+import time
+
+def run_job():
+    url = "https://flask-production-d5a3.up.railway.app/authorize"
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("Job executed successfully")
+    else:
+        print(f"Job failed with status code: {response.status_code}")
+
+# Schedule the job to run every hour
+schedule.every().hour.do(run_job)
+
+# Run the scheduler indefinitely
+while True:
+    schedule.run_pending()
+    time.sleep(1)
