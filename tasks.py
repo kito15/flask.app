da9cdd727723020e17e0a1be3281e34e3c8ec60b
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-07 12:16:57 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -95,8 +95,6 @@ def uploadFiles(self, serialized_credentials, recordings, accountName, email):
 
                         if accountName in topics:
                             """share_folder_with_email(drive_service, folder_id, email)"""
-                            share_url = recording['share_url']  # Get the share_url from the recording
-                            """return share_url"""
 
                         # Check if a file with the same name already exists in the folder
                         query = f"name='{video_filename}' and '{folder_id}' in parents"
