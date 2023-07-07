9541b2b6651a53c1b6195d91d368c671c525dd77
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-06 21:30:21 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -2,7 +2,6 @@ import requests
 import io
 import os
 import tempfile
-from datetime import datetime
 from flask import Flask, redirect, request, Blueprint, current_app, session
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
@@ -28,7 +27,6 @@ API_VERSION = 'v3'
 
 # Create the Flow instance
 stored_params = []
-share_links=[]
 
 flow = Flow.from_client_secrets_file(
     CLIENT_SECRETS_FILE,
@@ -64,99 +62,6 @@ def store_parameters(accountName,email):
 def retrieve_parameters():
     global stored_params
     return stored_params
-
-def uploadFiles(drive_service):
-    access_token = session.get('zoom_access_token')
-    recordings = download_zoom_recordings(access_token)
-    
-    # Check if the "Automated Zoom Recordings" folder already exists
-    results = drive_service.files().list(
-        q="name='Automated Zoom Recordings' and mimeType='application/vnd.google-apps.folder'",
-        fields='files(id)',
-        spaces='drive'
-    ).execute()
-
-    if len(results['files']) > 0:
-        recordings_folder_id = results['files'][0]['id']
-    else:
-        # Create the "Automated Zoom Recordings" folder if it doesn't exist
-        file_metadata = {
-            'name': 'Automated Zoom Recordings',
-            'mimeType': 'application/vnd.google-apps.folder'
-        }
-        recordings_folder = drive_service.files().create(body=file_metadata, fields='id').execute()
-        recordings_folder_id = recordings_folder['id']
-        
-    for recording in recordings:
-        topics = recording['topic']
-        folder_name = topics.replace(" ", "_")  # Replacing spaces with underscores
-        folder_name = folder_name.replace("'", "\\'")  # Escape single quotation mark
-        folder_id = None
-        
-        # Check if the folder already exists within "Automated Zoom Recordings"
-        results = drive_service.files().list(
-            q=f"name='{folder_name}' and '{recordings_folder_id}' in parents and mimeType='application/vnd.google-apps.folder'",
-            fields='files(id)',
-            spaces='drive'
-        ).execute()
-
-        if len(results['files']) > 0:
-            folder_id = results['files'][0]['id']
-        else:
-            # Create the folder within "Automated Zoom Recordings" if it doesn't exist
-            file_metadata = {
-                'name': folder_name,
-                'mimeType': 'application/vnd.google-apps.folder',
-                'parents': [recordings_folder_id]
-            }
-            folder = drive_service.files().create(body=file_metadata, fields='id').execute()
-            folder_id = folder['id']
-
-        for files in recording['recording_files']:
-            start_time = recording['start_time']
-            start_datetime = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%SZ")
-            date_string = start_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Updated format
-            video_filename = f"{topics}_{date_string}.mp4"
-
-            if files['status'] == 'completed' and files['file_extension'] == 'MP4' and recording['duration']>=10:
-                download_url = files['download_url']
-                response = requests.get(download_url)
-                video_content = response.content
-                video_filename = video_filename.replace("'", "\\'")  # Escape single quotation mark
-                
-                params=retrieve_parameters()
-                accountName=params[0]
-                email=params[1]
-                
-                if accountName in topics:
-                    share_folder_with_email(drive_service, folder_id, email)
-                    share_url = recording['share_url']  # Get the share_url from the recording
-                    return share_url
-                
-                # Check if a file with the same name already exists in the folder
-                query = f"name='{video_filename}' and '{folder_id}' in parents"
-                existing_files = drive_service.files().list(
-                    q=query,
-                    fields='files(id)',
-                    spaces='drive'
-                ).execute()
-
-                if len(existing_files['files']) > 0:
-                    # File with the same name already exists, skip uploading
-                    print(f"Skipping upload of '{video_filename}' as it already exists.")
-                    continue
-
-                # Upload the video to the folder in Google Drive
-                file_metadata = {
-                    'name': video_filename,
-                    'parents': [folder_id]
-                }
-                media = MediaIoBaseUpload(io.BytesIO(video_content), mimetype='video/mp4')
-                drive_service.files().create(
-                    body=file_metadata,
-                    media_body=media,
-                    fields='id'
-                ).execute()
                     
 # Callback route after authentication
 @upload_blueprint.route('/upload_callback')
@@ -169,7 +74,14 @@ def upload_callback():
     # Create a Google Drive service instance using the credentials
     credentials = flow.credentials
     drive_service = build('drive', API_VERSION, credentials=credentials)
+
+    access_token = session.get('zoom_access_token')
+    recordings = download_zoom_recordings(access_token)
+
+    params=retrieve_parameters()
+    accountName=params[0]
+    email=params[1]
     
-    share_url=uploadFiles(drive_service)
+    uploadFiles(drive_service,recordings,accountName,email)
     
-    return redirect('/test?share_url=' + urllib.parse.quote_plus(share_url))
+    return "Recording are being uploaded"
