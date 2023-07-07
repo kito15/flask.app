449ca347a8b98e482708ad83e8b99401a71ac28c
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-07 13:01:55 -0400

    Update shared_folders.py

--- shared_folders.py
+++ shared_folders.py
@@ -1,7 +1,1 @@
-shared_folders = []
-
-def add_shared_folder(share_link):
-    shared_folders.append(share_link)
-
-def is_folder_shared(share_link):
-    return share_link in shared_folders
+account_share_links = {}
