2c65f1757ee3cc39e11fb334a30a17fe63dcf5b3
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-10 15:39:00 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -110,16 +110,15 @@ def uploadFiles(self, serialized_credentials, recordings, accountName, email):
                         response.raise_for_status()
                         video_content = response.content
                         video_filename = video_filename.replace("'", "\\'")  # Escape single quotation mark
-                        if accountName and email is not None :
-                            if any(accountName in element for element in topics):
-                                if accountName not in account_share_links:
-                                    share_link = share_folder_with_email(drive_service, folder_id, email)
-                                    if share_link:
-                                         account_share_links[accountName] = share_link
-                                         store_account_share_links()  # Store the updated dictionary in Redis
-                                    else:
-                                        # Account already exists in the dictionary
-                                        existing_share_link = account_share_links[accountName]
+                        if accountName is not None and email is not None :
+                            if any(accountName in element for element in recording['topic']):
+                                share_link = share_folder_with_email(drive_service, folder_id, email)
+                                if share_link:
+                                     account_share_links[accountName] = share_link
+                                     store_account_share_links()  # Store the updated dictionary in Redis
+                                else:
+                                    # Account already exists in the dictionary
+                                    existing_share_link = account_share_links[accountName]
 
                         # Check if a file with the same name already exists in the folder
                         query = f"name='{video_filename}' and '{folder_id}' in parents"
