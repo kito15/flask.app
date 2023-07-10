1ca8ef8f411d853d0610ef2d4c964ae4882c258e
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-09 20:46:33 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -1,10 +1,8 @@
 from flask import Flask, redirect, request, Blueprint
 from google_auth_oauthlib.flow import Flow
-from google.auth.transport.requests import Request
-from download import download_zoom_recordings
-from tasks import uploadFiles
-import pickle
+from google.oauth2.credentials import Credentials
 import os
+import redis
 
 upload_blueprint = Blueprint('upload', __name__)
 upload_blueprint.secret_key = '@unblinded2018'
@@ -22,58 +20,65 @@ SCOPES = [
     'https://www.googleapis.com/auth/userinfo.profile'
 ]
 
+# Redis configuration
+REDIS_URL = 'redis://default:2qCxa3AEmJTH61oG4oa8@containers-us-west-90.railway.app:7759'
+redis_client = redis.from_url(REDIS_URL)
+
 # Create the Flow instance
-def create_flow():
-    return Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE,
-        scopes=SCOPES,
-        redirect_uri='https://flask-production-d5a3.up.railway.app/upload_callback'  # Replace with your domain
-    )
+flow = Flow.from_client_secrets_file(
+    CLIENT_SECRETS_FILE,
+    scopes=SCOPES,
+    redirect_uri='https://flask-production-d5a3.up.railway.app/upload_callback'
+)
+
+
+# Function to store credentials in Redis
+def store_credentials(credentials):
+    access_token = credentials.token
+    refresh_token = credentials.refresh_token
+    expires_at = credentials.expiry.timestamp()
+    redis_client.set('google_access_token', access_token)
+    redis_client.set('google_refresh_token', refresh_token)
+    redis_client.set('expires_at', expires_at)
+
+
+# Function to load credentials from Redis
+def load_credentials():
+    access_token = redis_client.get('google_access_token')
+    refresh_token = redis_client.get('google_refresh_token')
+    expires_at = redis_client.get('expires_at')
+    if access_token and refresh_token and expires_at:
+        credentials = Credentials(
+            token=access_token,
+            refresh_token=refresh_token,
+            token_uri=flow.fetch_token_uri,
+            client_id=flow.client_config['client_id'],
+            client_secret=flow.client_config['client_secret']
+        )
+        credentials.expiry = datetime.fromtimestamp(float(expires_at))
+        return credentials
+    return None
+
 
 # Redirect user to Google for authentication
 @upload_blueprint.route('/')
 def index():
-    flow = create_flow()
     authorization_url, state = flow.authorization_url(
         access_type='offline',
         include_granted_scopes='true'
     )
-    return redirect(authorization_url) 
+    return redirect(authorization_url)
 
-def is_token_expired(credentials):
-    if not credentials or not credentials.valid:
-        return True
-    if credentials.expired and credentials.refresh_token:
-        return credentials.expired
-    return False
-
-def store_parameters(accountName,email):
-    global stored_params
-    stored_params=[accountName,email]
-
-def retrieve_parameters():
-    global stored_params
-    stored_params[0].credentials = flow.credentials
-    return stored_params
 
 # Callback route after authentication
 @upload_blueprint.route('/upload_callback')
 def upload_callback():
     authorization_code = request.args.get('code')
-    flow = create_flow()
     flow.fetch_token(authorization_response=request.url)
 
     # Create a Google Drive service instance using the credentials
     credentials = flow.credentials
-
-    if is_token_expired(credentials):
-        if credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            return "Refresh token is missing. Please authenticate again."
-
-    # Store the refreshed credentials
-    stored_params[0].credentials = credentials
+    store_credentials(credentials)
 
     recordings = download_zoom_recordings()
 
