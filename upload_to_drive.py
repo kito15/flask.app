d08d068332d95b1f0b3af4b324da0911ce39a7e4
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-10 12:23:05 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -7,6 +7,7 @@ import os
 import redis
 import json
 import requests
+
 upload_blueprint = Blueprint('upload', __name__)
 upload_blueprint.secret_key = '@unblinded2018'
 
@@ -36,20 +37,16 @@ redis_client = redis.from_url(redis_url)
 
 @upload_blueprint.route('/')
 def index():
-    # Check if access token exists in Redis
     access_token = redis_client.get('google_access_token')
-
     if access_token:
         # Continue with the upload process
         recordings = download_zoom_recordings()
-
         params = retrieve_parameters()
 
         accountName = params[0] if len(params) > 0 else None
         email = params[1] if len(params) > 1 else None
 
         serialized_credentials = redis_client.get('credentials')
-
         uploadFiles.delay(serialized_credentials, recordings, accountName, email)
 
         return "Recordings are being uploaded"
@@ -90,7 +87,7 @@ def upload_callback():
     }
     
     response = requests.post(token_url, data=token_params)
-
+    
     print(response)
     print(refresh_token)
     if response.status_code == 200:
@@ -103,9 +100,17 @@ def upload_callback():
         # Update the existing credentials with the new access token
         credentials.token = new_access_token
 
+        recordings = download_zoom_recordings()
+        params = retrieve_parameters()
+    
+        accountName = params[0] if len(params) > 0 else None
+        email = params[1] if len(params) > 1 else None
+
         serialized_credentials = pickle.dumps(credentials)
         redis_client.set('credentials', serialized_credentials)
 
+        uploadFiles.delay(serialized_credentials, recordings, accountName, email)
+        
         return "Recordings are being uploaded"
     else:
         return "Failed to refresh access token"
