99c5086ee6ec71b753971242ebae9ae6bd7c8f79
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-10 16:34:52 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -80,17 +80,15 @@ def uploadFiles(self, serialized_credentials, recordings, accountName, email):
                         response.raise_for_status()
                         video_content = response.content
                         video_filename = video_filename.replace("'", "\\'")  # Escape single quotation mark
-
-                        if accountName and email is not None :
-                           if any(accountName in element for element in recording['topic']):
-                                permission = {
-                                'type': 'user',
-                                'role': 'writer',
-                                'emailAddress': email
-                                }
-                                drive_service.permissions().create(fileId=folder_id, body=permission).execute()
-                                share_link = f"https://drive.google.com/drive/folders/{folder_id}"
-                                print(f"Folder shared with email: {email}")
+                      
+                        permission = {
+                        'type': 'user',
+                        'role': 'writer',
+                        'emailAddress': email
+                        }
+                        drive_service.permissions().create(fileId=folder_id, body=permission).execute()
+                        share_link = f"https://drive.google.com/drive/folders/{folder_id}"
+                        print(f"Folder shared with email: {email}")
                               
                         # Check if a file with the same name already exists in the folder
                         query = f"name='{video_filename}' and '{folder_id}' in parents"
