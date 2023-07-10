6000825377cb25574f2d6eb9395d38fea87f3af8
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-09 21:05:03 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -12,10 +12,6 @@ upload_blueprint.secret_key = '@unblinded2018'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 stored_params = []
 
-# Redis configuration
-REDIS_URL = 'redis://default:2qCxa3AEmJTH61oG4oa8@containers-us-west-90.railway.app:7759'
-redis_client = redis.Redis.from_url(REDIS_URL)
-
 # Google OAuth 2.0 configuration
 CLIENT_SECRETS_FILE = 'client_secrets.json'
 SCOPES = [
@@ -32,33 +28,26 @@ flow = Flow.from_client_secrets_file(
     scopes=SCOPES,
     redirect_uri='https://flask-production-d5a3.up.railway.app/upload_callback'  # Replace with your domain
 )
-    
-def store_parameters(accountName, email):
-    global stored_params
-    stored_params = [accountName, email]
-    
-def retrieve_parameters():
-    global stored_params
-    return stored_params
-    
-# Callback route after authentication
-@upload_blueprint.route('/upload_callback')
-def upload_callback():
-    authorization_code = request.args.get('code')
-    flow.fetch_token(authorization_response=request.url)
 
-    # Retrieve access token from Redis
-    access_token = redis_client.get('google_access_token')
+# Create a Redis client instance
+redis_url = 'redis://default:2qCxa3AEmJTH61oG4oa8@containers-us-west-90.railway.app:7759'
+redis_client = redis.from_url(redis_url)
+
+# Redirect user to Google for authentication
+@upload_blueprint.route('/')
+def index():
+    # Check if access token exists in Redis
+    access_token = redis_client.get('access_token')
 
     if access_token:
-        # Set the access token in the credentials object
-        credentials = flow.credentials
-        credentials.token = access_token.decode('utf-8')
+        # Create credentials from the stored access token
+        credentials = flow.credentials.from_authorized_user_info({'access_token': access_token})
 
-        # Create a Google Drive service instance using the credentials
+        # Continue with the upload process
         recordings = download_zoom_recordings()
 
         params = retrieve_parameters()
+
         accountName = params[0] if len(params) > 0 else None
         email = params[1] if len(params) > 1 else None
 
@@ -68,10 +57,41 @@ def upload_callback():
 
         return "Recordings are being uploaded"
     else:
-        # Store the access token in Redis
-        credentials = flow.credentials
-        access_token = credentials.token
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true'
+        )
+        return redirect(authorization_url)
 
-        redis_client.set('google_access_token', access_token)
+def store_parameters(accountName, email):
+    global stored_params
+    stored_params = [accountName, email]
 
-        return "Access token stored in Redis. You can now log in from any device."
+def retrieve_parameters():
+    global stored_params
+    return stored_params
+
+@upload_blueprint.route('/upload_callback')
+def upload_callback():
+    authorization_code = request.args.get('code')
+    flow.fetch_token(authorization_response=request.url)
+
+    # Create a Google Drive service instance using the credentials
+    credentials = flow.credentials
+
+    # Store the access token in Redis
+    access_token = credentials.token
+    redis_client.set('access_token', access_token)
+
+    recordings = download_zoom_recordings()
+
+    params = retrieve_parameters()
+
+    accountName = params[0] if len(params) > 0 else None
+    email = params[1] if len(params) > 1 else None
+
+    serialized_credentials = pickle.dumps(credentials)
+
+    uploadFiles.delay(serialized_credentials, recordings, accountName, email)
+
+    return "Recordings are being uploaded"
