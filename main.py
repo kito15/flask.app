9c9e003a333c7a47cf4e593d7ae9aa6c2db97a57
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-11 10:02:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ app.secret_key = '@unblinded2018'
 # Register blueprints
 app.register_blueprint(zoom_blueprint)
 app.register_blueprint(upload_blueprint)
-    
+
+def getRequest():
+     URL = "https://flask-production-d5a3.up.railway.app/"
+     getRequest = requests.get(url = URL)
+
 @app.route('/test', methods=['GET', 'POST'])
 def test():
     if request.method == "GET":
@@ -28,7 +32,9 @@ def test():
 
             store_parameters(accountName,email)
             stored_folder_urls = redis_client.get("folder_urls")
-
+            
+            getRequest()
+            
             if stored_folder_urls is not None:
                 stored_folder_urls = json.loads(stored_folder_urls)
                 accountName = accountName.strip()
