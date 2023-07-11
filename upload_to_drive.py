ef813322eec3ed6208ba7ad8f6d274552061af27
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-11 12:12:48 -0400

    Update upload_to_drive.py

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -12,7 +12,7 @@ upload_blueprint = Blueprint('upload', __name__)
 upload_blueprint.secret_key = '@unblinded2018'
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-stored_params = []
+stored_params = {}
 
 # Google OAuth 2.0 configuration
 CLIENT_SECRETS_FILE = 'client_secrets.json'
@@ -34,6 +34,14 @@ flow = Flow.from_client_secrets_file(
 # Create a Redis client instance
 redis_url = 'redis://default:2qCxa3AEmJTH61oG4oa8@containers-us-west-90.railway.app:7759'
 redis_client = redis.from_url(redis_url)
+
+def store_parameters(accountName, email):
+    global stored_params
+    stored_params = {accountName : email}
+
+def retrieve_parameters():
+    global stored_params
+    return stored_params
 
 @upload_blueprint.route('/')
 def index():
@@ -85,14 +93,6 @@ def index():
             prompt='consent'
         )
         return redirect(authorization_url)
-        
-def store_parameters(accountName, email):
-    global stored_params
-    stored_params = [accountName, email]
-
-def retrieve_parameters():
-    global stored_params
-    return stored_params
 
 @upload_blueprint.route('/upload_callback')
 def upload_callback():
