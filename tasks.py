b019580c32a5261d555239afc63fad27fa52a915
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-15 22:47:48 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -95,7 +95,7 @@ def uploadFiles(self, serialized_credentials, recordings):
                 date_string = start_datetime.strftime("%Y-%m-%d_%H-%M-%S")  # Updated format
                 video_filename = f"{topics}_{date_string}.mp4"
                 download_url = files['download_url']
-               
+            
                 if files['status'] == 'completed' and files['file_extension'] == 'MP4' and recording['duration'] >= 10:
                     try:
                         response = requests.get(download_url, stream=True)
