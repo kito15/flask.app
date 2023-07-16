1eca23d6e890607543d9d6256400740261d700c1
Ramon Inoa <97205364+kito15@users.noreply.github.com>
2023-07-15 22:11:27 -0400

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,6 +5,7 @@ from celery import Celery
 from datetime import datetime
 import urllib.parse
 from google_auth_oauthlib.flow import Flow
+import redis
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from google.oauth2 import credentials as google_credentials
